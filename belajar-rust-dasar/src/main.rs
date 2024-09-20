0da06ce02c25af22c2e31c5565717cca609f9ef9
Rifky Syahreza <rsyahrezafs@gmail.com>
2024-09-20 18:43:54 +0700

    commit lagi

--- belajar-rust-dasar/src/main.rs
+++ belajar-rust-dasar/src/main.rs
@@ -197,6 +197,11 @@ fn tuple_test(){
     let data_explisit: (i32, f64, bool) = (10, 10.5, true);
     println!("the tuple is {:?}", data_explisit);
 
+    // destructuring tuple
+
+    // index ke 3 tidak terpakai
+    let (x, y, z, _) = a;
+    
     let (x, y, z, w) = a;
     println!("the x is {}", x);
     println!("the y is {}", y);
@@ -207,4 +212,210 @@ fn tuple_test(){
     println!("the y is {}", a.1);
     println!("the z is {}", a.2);
     println!("the w is {}", a.3);
+
+    // mutable tuple
+    let mut b = (1, 2.5, 'a', true);
+    b.0 = 10;
+    println!("the tuple is {:?}", b);
 }
+
+// Unit Type
+/*
+    Unit type adalah tipe data yang tidak memiliki nilai
+    Unit type di rust menggunakan tanda kurung
+    Unit type biasanya digunakan untuk fungsi yang tidak mengembalikan nilai
+*/
+fn unit() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn unit_type_test(){
+    let a = ();
+    println!("the unit type is {:?}", a);
+
+    let result = unit();
+    println!("the unit type is {:?}", result);
+}
+
+// Array
+/*
+    Array adalah kumpulan nilai dengan tipe data yang sama
+    Array di rust menggunakan tanda kurung siku
+    Array di rust memiliki jumlah data yang final
+    Array di rust bisa diakses menggunakan index
+*/
+#[test]
+fn array_test(){
+    let a = [1, 2, 3, 4, 5];
+    println!("the array is {:?}", a);
+
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("the array is {:?}", b);
+
+    let c = [0; 5];
+    println!("the array is {:?}", c);
+
+    let d = [1, 2, 3, 4, 5];
+    println!("the array is {:?}", d[0]);
+    println!("the array is {:?}", d[1]);
+    println!("the array is {:?}", d[2]);
+    println!("the array is {:?}", d[3]);
+    println!("the array is {:?}", d[4]);
+
+    // mutable array
+    let mut e = [1, 2, 3, 4, 5];
+    e[0] = 10;
+    println!("the array is {:?}", e);
+
+    let length = e.len();
+    println!("the array length is {:?}", length);
+}
+
+// Two-Dimensional Array
+#[test]
+fn two_dimensional_array_test(){
+    let matrix: [[i32; 3]; 3] = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
+
+    println!("the matrix is {:?}", matrix);
+    println!("{}", matrix[0][0]);
+    println!("{}", matrix[0][1]);
+    println!("{}", matrix[0][2]);
+
+    let length = matrix.len();
+    println!("the matrix length is {}", length);
+
+    let length_inner_array = matrix[0].len();
+    println!("the matrix length is {}", length_inner_array); 
+}
+
+// Constants
+/*
+    Rust mendukung konstanta
+    Konstanta di rust menggunakan keyword const
+    Konstanta di rust menggunakan huruf besar
+    Konstanta di rust harus mendeklarasikan tipedata
+    Konstanta di rust harus mendeklarasikan nilai
+    Konstanta di rust tidak bisa menggunakan mutable
+*/
+#[test]
+fn constant_test(){
+    const MAX_POINTS: u32 = 100_000;
+    println!("the constant is {}", MAX_POINTS);
+}
+
+// Variable Scope
+/*
+    Rust mendukung variable scope
+    Variable scope adalah ruang lingkup variable
+    Variable scope di rust menggunakan kurung kurawal
+    Variable scope di rust bisa menggunakan variable yang sama
+    Variable scope di rust bisa menggunakan variable yang berbeda
+*/
+#[test]
+fn variable_scope_test(){
+    let a = 10; // variable a di scope ini
+
+    { // inner scope
+        let b = 20;
+        println!("the b is {}", b);
+
+        println!("the b is {}", a); // variable a di scope ini
+    }
+
+    println!("the a is {}", a);
+}
+
+//============= Memory Management =============
+
+// Garbage Collection
+/*
+    Rust tidak menggunakan garbage collection
+    Rust menggunakan sistem ownership
+    Rust menggunakan sistem ownership untuk mengelola memory
+    Rust menggunakan sistem ownership untuk menghindari memory leak
+    Rust menggunakan sistem ownership untuk menghindari double free
+*/
+
+// Stack and Heap
+/*
+    Rust menggunakan stack dan heap untuk mengelola memory
+    Stack adalah memory yang cepat dan kecil
+    Stack digunakan untuk data yang ukurannya sudah diketahui
+    Stack digunakan untuk data yang ukurannya kecil
+    Stack digunakan untuk data yang tidak bisa dihapus
+    Stack digunakan untuk data yang tidak bisa diubah
+    Stack digunakan untuk data yang tidak bisa diakses secara acak
+    Stack digunakan untuk data yang bisa diakses secara cepat
+    Stack digunakan untuk data yang bisa diakses secara berurutan
+    Stack digunakan untuk data yang bisa diakses secara sekuensial
+    Stack digunakan untuk data yang bisa diakses secara linear
+    Stack digunakan untuk data yang bisa diakses secara langsung
+    Stack digunakan untuk data yang bisa diakses secara sejajar
+    Stack digunakan untuk data yang bisa diakses secara berurutan
+*/
+/*
+    Heap adalah memory yang lambat dan besar
+    Heap digunakan untuk data yang ukurannya belum diketahui
+    Heap digunakan untuk data yang ukurannya besar
+    Heap digunakan untuk data yang bisa dihapus
+    Heap digunakan untuk data yang bisa diubah
+    Heap digunakan untuk data yang bisa diakses secara acak
+    Heap digunakan untuk data yang bisa diakses secara lambat
+    Heap digunakan untuk data yang bisa diakses secara tidak berurutan
+    Heap digunakan untuk data yang bisa diakses secara tidak sekuensial
+    Heap digunakan untuk data yang bisa diakses secara tidak linear
+    Heap digunakan untuk data yang bisa diakses secara tidak langsung
+    Heap digunakan untuk data yang bisa diakses secara tidak sejajar
+    Heap digunakan untuk data yang bisa diakses secara tidak berurutan
+    Anggap saja stack adalah tumpukan buku, sedangkan heap adalah rak buku
+*/
+#[test]
+fn stack_and_heap() {
+    function_a();
+    function_b();
+}
+
+fn function_a(){
+    let a = 10; // stack
+    let b = String::from("Hello"); // heap
+
+    println!("{} {}", a, b);
+}
+
+fn function_b() {
+    let a = 20; // stack
+    let b = String::from("World"); // heap
+
+    println!("{} {}", a, b);
+}
+
+// Drop Function
+/*
+    Saat variable keluar dari scope nya, yang artinya variable sudah tidak digunakan lagi
+    Rust akan memanggil drop function
+    Drop function adalah fungsi yang akan menghapus data di heap
+    Dan jika rust function() sudah selesai dieksekusi, maka function()
+    tersebut akan dihapus pula dari stack frame
+*/
+
+// String Type
+/*
+    Rust memiliki tipe data text yang fixed size, yaitu &str (string slice)
+    dan yang bisa mengembang ukurannya, yaitu String
+    &str karena ukurannya fixed size, jadi Rust akan menyimpannya di stack,
+    sedangkan String karena ukurannya bisa mengembang, Rust akan menyimpannya di heap
+*/
+
+// Immutable str
+/*
+    Karena ukuran &str fixed size, maka &str tidak bisa diubah/immutable
+    ketika kita buat variable mutable, dan mengubah data &str, sebenarnya yang
+    dilakukan adalah mengganti isi variable, bukan mengubah isi dari &str nya
+    &str memiliki banyak method yang bisa digunakan, dan beberapa method
+    tersebut mengembalikan &str baru atau ada yang String
+*/
